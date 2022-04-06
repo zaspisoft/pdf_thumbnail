45425f2e39cd6ca187008ac2a342444b66afb4a2
akazyti <akazyti@gmail.com>
2022-04-06 07:14:55 +0000

    fix: path issues

--- pdf_thumbnail/__init__.py
+++ pdf_thumbnail/__init__.py
@@ -1,5 +1,6 @@
 from pdf2image import convert_from_path
 from frappe.utils.file_manager import save_url
+import frappe
 
 __version__ = '1.0.0'
 
@@ -14,12 +15,17 @@ def get_file_path(doc):
 
 
 def generate_thumbnail(doc, event=None):
+
+	if not doc.file_name.endswith('pdf'):
+		return
+	
 	file_path = get_file_path(doc)
 	pages = convert_from_path(file_path, 500)
 	if len(pages) >= 1:
-		image_path = "{}/{}_thumbnail.jpeg".fromat(frappe.get_site_path(), doc.name)
+		image_path = "{}/public/files/{}_thumbnail.jpeg".format(frappe.get_site_path(), doc.name)
+		url_path = "/files/{}_thumbnail.jpeg".format(doc.name)
 		pages[0].save(image_path, 'JPEG')
-		frappe.db.set_value('File', doc.name, 'image', image_path)
+		frappe.db.set_value('File', doc.name, 'image', url_path)
 		frappe.db.commit()
 
 
